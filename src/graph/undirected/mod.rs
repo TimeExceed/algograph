d8c28b62c5c035f0ca0156fe80d27c9c529fa077
Taoda <tyf00@aliyun.com>
2023-05-05 22:50:17 +0800

    move tests around

--- src/graph/undirected/mod.rs
+++ src/graph/undirected/mod.rs
@@ -4,3 +4,25 @@ mod tree_backed;
 pub use self::tree_backed::*;
 mod adjacent_list;
 pub use self::adjacent_list::*;
+
+#[cfg(test)]
+mod tests {
+    use crate::graph::*;
+
+    #[test]
+    fn to_graphviz() {
+        let mut g = undirected::AdjacentListGraph::new();
+        let v = g.add_vertex();
+        g.add_edge(v, v);
+        let trial = {
+            let mut trial = vec![];
+            g.dump_in_graphviz(&mut trial, "trial").unwrap();
+            String::from_utf8(trial).unwrap()
+        };
+        assert_eq!(trial, r#"graph trial {
+  0 ;
+  0 -- 0 ;
+}
+"#);
+    }
+}
